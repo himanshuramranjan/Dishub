f866442e9a96e0fe9606ccfcea7d03d0d35da279
himanshuramranjan <himanshuramr@gmail.com>
2023-05-03 15:08:35 +0530

    Global Controller added

---
+++ controller/globalController.js
@@ -0,0 +1,82 @@
+const catchAsyncError = require('../utils/catchAsyncError');
+
+// Get All the docs
+exports.getAll = Model => catchAsyncError(async (req, res, next) => {
+
+    const docs = await Model.find();
+
+    res.status(200).json({
+        status: 'Success',
+        result: docs.length,
+        data: {
+            docs
+        }
+    });
+});
+
+// Get a single doc
+exports.getOne = Model => catchAsyncError(async (req, res, next) => {
+    
+    const doc = await Model.findById(req.params.id);
+
+    if(!doc) {
+        return next(new AppError(`No doc w/ Id: ${req.params.id} exist`, 404));
+    }
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            doc
+        }
+    });
+});
+
+// Create a new doc
+exports.createOne = Model => catchAsyncError(async (req, res, next) => {
+
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+        status: 'Success',
+        data: {
+            doc
+        }
+    });
+});
+
+// Update the doc
+exports.updateOne = Model => catchAsyncError(async (req, res, next) => {
+
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    if(!doc) {
+        return next(new AppError(`No Doc w/ Id: ${req.params.id} exist`, 404));
+    }
+
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            doc
+        }
+    });
+});
+
+// Delete the doc
+exports.deleteOne = Model => catchAsyncError(async (req, res, next) => {
+    
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if(!doc) {
+        return next(new AppError(`No Doc w/ Id: ${req.params.id} exist`, 404));
+    }
+
+    res.status(204).json({
+        status: 'Success',
+        message: 'Doc Deleted Successfully'
+    });
+});
+
+
--- controller/postController.js
+++ controller/postController.js
@@ -1,83 +1,21 @@
 const Post = require('../model/postModel');
 const catchAsyncError = require('../utils/catchAsyncError');
+const globalController = require('../controller/globalController');
+
 const AppError = require('../utils/appError');
 
 
 // Get All Posts
-exports.getAllPosts = catchAsyncError(async (req, res, next) => {
-
-    const posts = await Post.find();
-
-    res.status(200).json({
-        status: 'Success',
-        result: posts.length,
-        data: {
-            posts
-        }
-    });
-});
+exports.getAllPosts = globalController.getAll(Post);
 
 // Create a new Post
-exports.createPost = catchAsyncError(async (req, res, next) => {
-
-    const post = await Post.create(req.body);
-
-    res.status(201).json({
-        status: 'Success',
-        data: {
-            post
-        }
-    });
-});
+exports.createPost = globalController.createOne(Post);
 
 // Get a single post
-exports.getPost = catchAsyncError(async (req, res, next) => {
-    
-    const post = await Post.findById(req.params.id);
-
-    if(!post) {
-        return next(new AppError(`No Post w/ Id: ${req.params.id} exist`, 404));
-    }
-
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            post
-        }
-    });
-});
+exports.getPost = globalController.getOne(Post);
 
 // Update the post
-exports.updatePost = catchAsyncError(async (req, res, next) => {
-
-    const post = Post.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    });
-
-    if(!post) {
-        return next(new AppError(`No Post w/ Id: ${req.params.id} exist`, 404));
-    }
-
-    res.status(200).json({
-        status: 'Success',
-        data: {
-            post
-        }
-    });
-});
+exports.updatePost = globalController.updateOne(Post);
 
 // Delete a Post
-exports.deletePost = catchAsyncError(async (req, res, next) => {
-    
-    const post = await Post.findByIdAndDelete(req.params.id);
-
-    if(!post) {
-        return next(new AppError(`No Post w/ Id: ${req.params.id} exist`, 404));
-    }
-
-    res.status(204).json({
-        status: 'Success',
-        message: 'Post Deleted Successfully'
-    });
-});
+exports.deletePost = globalController.deleteOne(Post);
