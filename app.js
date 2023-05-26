7ef0d0d606847a908bc819c6be446b17b85a1afd
himanshuramranjan <himanshuramr@gmail.com>
2023-05-26 10:46:28 +0530

    Security features added

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
 
 const postRouter = require('./routes/postRoutes');
 const commentRouter = require('./routes/commentRoutes');
@@ -6,11 +11,41 @@ const creatorRouter = require('./routes/creatorRoutes');
 
 const errorHandler = require('./controller/errorController');
 const AppError = require('./utils/AppError');
+const { mongo } = require('mongoose');
 
 const app = express();
 
+// define max api req (to max 100 req / hour from a given IP)
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in 1 hour' 
+});
+
 // Middleware
+
+// set security http headers
+app.use(helmet());
+
+// limits the api request
+app.use('/api', limiter);
+
+// parse the data from body into req.body (with 10kb data limit)
 app.use(express.json({ limit: '10kb'}));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xssClean());
+
+// prevent http parameter pollution
+app.use(hpp({
+    whitelist: [
+        'domain',
+        'creator'
+    ]
+}));
 
 // Routes
 app.use('/api/v1/dishub/posts', postRouter);
